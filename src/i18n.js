94256e1bc17961bdf8f9c77f423d07b3af92b6fc
adamu-tako <amusatako@gmail.com>
2025-12-15 03:46:50 +0100

    Remove package-lock.json and update README.md to reflect a modern React starter template with TypeScript, Tailwind CSS, and other key features. Adjust i18n configuration for default language settings.

--- src/i18n.js
+++ src/i18n.js
@@ -5,18 +5,16 @@ import { initReactI18next } from 'react-i18next';
 import en from './locales/en.json';
 import zh from './locales/zh.json';
 
-i18n
-  .use(initReactI18next)  
-  .init({
-    resources: {
-      en: { translation: en },
-      zh: { translation: zh },
-    },
-    lng: 'zh',  
-    fallbackLng: 'en', // When no lang
-    interpolation: {
-      escapeValue: false, // React avoid XSS
-    },
-  });
+i18n.use(initReactI18next).init({
+	resources: {
+		en: { translation: en },
+		zh: { translation: zh },
+	},
+	lng: 'en',
+	fallbackLng: 'en', // When no lang
+	interpolation: {
+		escapeValue: false, // React avoid XSS
+	},
+});
 
 export default i18n;
